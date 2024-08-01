6b4218a4f516104c51f873eb58c7313a19dd0410
TanyaSingh103 <tanya.103.singh@gmail.com>
2024-08-01 16:29:18 +0530

    initial structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export function Navbar(){
     return(
       <div className="fixed left-0 top-5 z-50 w-full">
-      <nav className="text-text border-border dark:border-darkBorder shadow-light dark:shadow-dark mx-auto flex w-max gap-5 rounded-base border-2 bg-main p-2.5 px-5 text-sm font-base sm:text-base w450:gap-4">
-      this is the navbar
+      <nav className="border-black shadow-light mx-auto flex w-max gap-5 border-2 bg-orange-500 p-2.5 px-5 text-base">
+      this is the navbar qwertyuiopasdfghjklzxcvbnm
       </nav>
     </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,11 @@ export default function HomePage() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-5xl font-bold">Welcome to My Portfolio</h1>
       <p className="mt-4 text-xl">I am trying to be a web developer specializing in React and Next.js.</p>
+      
     
+          
       <Navbar />
+               
     </div>
   )
 }
