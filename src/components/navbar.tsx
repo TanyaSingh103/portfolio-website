375f0e6c531b95d8eb877dca5f24517a5ddb13bc
TanyaSingh103 <tanya.103.singh@gmail.com>
2024-08-01 18:16:41 +0530

    home page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 export function Navbar(){
     return(
       <div className="fixed left-0 top-5 z-50 w-full">
-      <nav className="border-black shadow-light mx-auto flex w-max gap-5 border-2 bg-orange-500 p-2.5 px-5 text-base">
-      this is the navbar qwertyuiopasdfghjklzxcvbnm
+      <nav className="border border-box border-black border-t-4 border-l-4 border-b-8 border-r-8 rounded-lg bg-emerald-400 mx-auto flex w-max gap-5 p-3 px-5 text-2xl font-semibold">
+      <button type="button" className="border-transparent hover:border-black border-4 rounded-lg text-center p-2 px-3">Home</button>
+      <button type="button" className="border-transparent hover:border-black border-4 rounded-lg text-center p-2 px-3">About</button>
+      <button type="button" className="border-transparent hover:border-black border-4 rounded-lg text-center p-2 px-3">Experience</button>
+      <button type="button" className="border-transparent hover:border-black border-4 rounded-lg text-center p-2 px-3">Projects</button>
       </nav>
     </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Navbar } from '../components/navbar';
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-5xl font-bold">Welcome to My Portfolio</h1>
-      <p className="mt-4 text-xl">I am trying to be a web developer specializing in React and Next.js.</p>
-      
+    <div className="min-h-screen flex flex-col items-left justify-center bg-gray-100 p-60 pt-40">
+      <p className="mt-4 text-xl pb-4">Hi, my name is</p>
+      <h1 className="text-7xl font-bold pb-6">Tanya Singh</h1>
+      <p className="mt-4 text-3xl">I'm a front-end developer </p>
+      <p className="mt-4 text-2xl pb-4">also involved with machine learning</p>
+
     
           
       <Navbar />
