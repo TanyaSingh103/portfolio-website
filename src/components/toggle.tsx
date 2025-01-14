8caf093c947a67f7054de23d5023b213164dca1b
TanyaSingh103 <tanya.103.singh@gmail.com>
2025-01-14 18:51:29 +0530

    theme

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -1,49 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { MoonIcon, SunIcon } from '@heroicons/react/outline';
+import { MoonIcon, SunIcon } from '@heroicons/react/outline'; 
 
 const ThemeSwitcher: React.FC = () => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
-  const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
-
+  
   useEffect(() => {
     const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      setIsDarkMode(storedTheme === 'dark');
-    } else {
-      // Default to dark mode
+    if (storedTheme === 'dark') setIsDarkMode(true);
+  }, []);
+  
+  useEffect(() => {
+    if (isDarkMode) {
       document.documentElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
-      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
-    setIsInitialized(true); // Prevent showing the wrong icon before the state is initialized
-  }, []);
-
-  useEffect(() => {
-    if (isInitialized) {
-      if (isDarkMode) {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('theme', 'light');
-      }
-    }
-  }, [isDarkMode, isInitialized]);
+  }, [isDarkMode]);
+  
+   
 
   return (
     <button
       type="button"
-      onClick={toggleDarkMode}
+      onClick={toggleDarkMode} 
       className="border-transparent text-center p-2 px-1 transition-all duration-300 ease-in-out"
     >
       {isDarkMode ? (
-        <SunIcon className="h-8 w-8 text-white" /> // Sun icon for light mode
+        <SunIcon className="h-8 w-8 text-black-500" />
       ) : (
-        <MoonIcon className="h-8 w-8 text-black" /> // Moon icon for dark mode
+        <MoonIcon className="h-8 w-8 text-black-900" />
       )}
     </button>
   );
